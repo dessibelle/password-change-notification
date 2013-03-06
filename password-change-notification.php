5541c0eaa1f9b348ecd9b3404342cf99b479c715
Simon Fransson <simon@dessibelle.se>
2013-03-06 13:37:59 +0100

    Added filters for customization of messages etc.

--- password-change-notification.php
+++ password-change-notification.php
@@ -4,7 +4,7 @@ Plugin Name: Password Change Notification
 Plugin URI: http://dessibelle.se
 Description: A WordPress plugin for sending email notifications when passwords change
 Author: Simon Fransson
-Version: 1.0
+Version: 1.0b1
 Author URI: http://dessibelle.se
 */
 
@@ -34,8 +34,8 @@ class PasswordChangeNotification {
     public function profile_update($user_id, $old_user_data = null)
     {
 
-        $name = implode(" ", array_filter(array($_POST['first_name'], $_POST['last_name'])));
-        $email = $_POST['email'];
+        $name = apply_filters('pwcn_to_name', implode(" ", array_filter(array($_POST['first_name'], $_POST['last_name']))));
+        $email = $to = apply_filters('pwcn_to_email', $_POST['email'], $user_id);;
         $password = $_POST['pass1'];
 
         if (!empty($password)) {
@@ -43,15 +43,16 @@ class PasswordChangeNotification {
             if (!empty($name) && !empty($to)) {
                 $to = sprintf('%s <%s>', $name, $email);
             }
+            $to = apply_filters('pwcn_to_header', $to, $user_id);
 
-            $site_url = get_bloginfo('home');
-            $login_url = wp_login_url();
-            $site_name = get_bloginfo('name');
-            $host = parse_url($site_url, PHP_URL_HOST);
+            $site_url = apply_filters('pwcn_site_url', get_bloginfo('home'), $user_id);
+            $login_url = apply_filters('pwcn_login_url', wp_login_url(apply_filters('pwcn_login_url_redirect', null, $user_id)), $user_id);
+            $site_name = apply_filters('pwcn_site_name', get_bloginfo('name'), $user_id);
+            $host = apply_filters('pwcn_site_hostname', parse_url($site_url, PHP_URL_HOST), $user_id);
 
-            $subject = sprintf(__('New password for %s', 'pwcn'), $host);
+            $subject = apply_filters('pwcn_subject', sprintf(__('New password for %s', 'pwcn'), $host), $user_id);
             $message_format = __("Hi %s,\n\nyour password for %s (%s) has changed. The new password is: \n\n%s\n\nTo login, go to %s.", 'pwcn');
-            $message = sprintf($message_format, $_POST['first_name'], $site_name, $site_url, $password, $login_url);
+            $message = apply_filters('pwcn_message', sprintf($message_format, $_POST['first_name'], $site_name, $site_url, $password, $login_url), $user_id);
 
             wp_mail( $to, $subject, $message);
         }
